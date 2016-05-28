30a3532a53a28c4ec016176e2718254aafe33ce3
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:34:04 +0300

    add class Aster and Tulip

---
+++ src/com/goit/javaonline/flowers/Aster.java
@@ -0,0 +1,16 @@
+package com.goit.javaonline.flowers;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class Aster extends Flower {
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/com/goit/javaonline/flowers/Tulip.java
@@ -0,0 +1,16 @@
+package com.goit.javaonline.flowers;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class Tulip extends Flower {
+    private String countryOrigin;
+
+    public String getCountryOrigin() {
+        return countryOrigin;
+    }
+
+    public void setCountryOrigin(String countryOrigin) {
+        this.countryOrigin = countryOrigin;
+    }
+}
