a6c5e2ff9dd5b6d3cd9634e47351f1e6c6b1f36f
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:15:36 +0300

    add class TextFile

---
+++ src/com/goit/javaonline/files/TextFile.java
@@ -0,0 +1,16 @@
+package com.goit.javaonline.files;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class TextFile extends File {
+    private String encoding;
+
+    public String getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+}
