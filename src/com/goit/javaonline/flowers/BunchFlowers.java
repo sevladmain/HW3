50b393e980d8795dfeeff84465da0af0a485ee36
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:46:41 +0300

    add class BunchFlower

---
+++ src/com/goit/javaonline/flowers/BunchFlowers.java
@@ -0,0 +1,19 @@
+package com.goit.javaonline.flowers;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+
+import java.util.List;
+
+public class BunchFlowers {
+    private List<Flower> flowers;
+
+    public List<Flower> getFlowers() {
+        return flowers;
+    }
+
+    public void setFlowers(List<Flower> flowers) {
+        this.flowers = flowers;
+    }
+}
---
+++ src/com/goit/javaonline/flowers/RoseBush.java
@@ -0,0 +1,18 @@
+package com.goit.javaonline.flowers;
+
+import java.util.List;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class RoseBush {
+    private List<Rose> roses;
+
+    public List<Rose> getRoses() {
+        return roses;
+    }
+
+    public void setRoses(List<Rose> roses) {
+        this.roses = roses;
+    }
+}
