1aaa0ffa79a6cf73f90f699aac6e0676d1b91f7e
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:14:41 +0300

    add class ImageFile

---
+++ src/com/goit/javaonline/files/ImageFile.java
@@ -0,0 +1,16 @@
+package com.goit.javaonline.files;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class ImageFile extends File{
+    private int resolution;
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(int resolution) {
+        this.resolution = resolution;
+    }
+}
