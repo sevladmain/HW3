9ad947ca54d8c9de359941369cd9cfec94501cb9
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:17:37 +0300

    add class AudioFile

---
+++ src/com/goit/javaonline/files/AudioFile.java
@@ -0,0 +1,16 @@
+package com.goit.javaonline.files;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class AudioFile extends File {
+    private int bitRate;
+
+    public int getBitRate() {
+        return bitRate;
+    }
+
+    public void setBitRate(int bitRate) {
+        this.bitRate = bitRate;
+    }
+}
