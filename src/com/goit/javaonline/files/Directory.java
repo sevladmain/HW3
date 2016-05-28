c733f686a7052157e44d5dd96989a2c620d3f1bf
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:24:50 +0300

    add class Directory

---
+++ src/com/goit/javaonline/files/Directory.java
@@ -0,0 +1,25 @@
+package com.goit.javaonline.files;
+
+import java.util.List;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class Directory {
+    private List<File> fileList;
+
+    public List<File> getFileList() {
+        return fileList;
+    }
+
+    public void setFileList(List<File> fileList) {
+        this.fileList = fileList;
+    }
+
+    public void deleteFile(File f){
+
+    }
+    public void createFile(File f){
+
+    }
+}
