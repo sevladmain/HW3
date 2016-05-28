9dbe43f1ee148391826a682d9cc3a07f9670f834
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:13:43 +0300

    add class File

---
+++ src/com/goit/javaonline/files/File.java
@@ -0,0 +1,25 @@
+package com.goit.javaonline.files;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class File {
+    private String fileName;
+    private int fileSize;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+}
