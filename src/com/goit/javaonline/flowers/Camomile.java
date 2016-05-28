1f32142da61131dbb01fe20d10215a72645a2dd7
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:30:44 +0300

    add class Camomile and Rose

---
+++ src/com/goit/javaonline/flowers/Camomile.java
@@ -0,0 +1,16 @@
+package com.goit.javaonline.flowers;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class Camomile extends Flower {
+    private int height;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
---
+++ src/com/goit/javaonline/flowers/Rose.java
@@ -0,0 +1,16 @@
+package com.goit.javaonline.flowers;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class Rose extends Flower {
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
