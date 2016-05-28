9c483ffde28fd345ebb451683f9ffa92df6c190c
sevladmain <sevlad.main@gmail.com>
2016-05-28 12:27:26 +0300

    add class Flower

---
+++ src/com/goit/javaonline/flowers/Flower.java
@@ -0,0 +1,16 @@
+package com.goit.javaonline.flowers;
+
+/**
+ * Created by SeVlad on 28.05.2016.
+ */
+public class Flower {
+    private String latinName;
+
+    public String getLatinName() {
+        return latinName;
+    }
+
+    public void setLatinName(String latinName) {
+        this.latinName = latinName;
+    }
+}
