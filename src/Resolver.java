77ff60f97aec06291f1fdba1b57502703d5cc679
sevladmain <sevlad.main@gmail.com>
2016-06-03 00:25:57 +0300

    finish module4

--- src/Resolver.java
+++ src/Resolver.java
@@ -1,3 +1,4 @@
+import com.goit.javaonline.module4.CalcLength;
 import com.goit.javaonline.module4.CalcSquare;
 import com.goit.javaonline.module4.TemperatureTransform;
 
@@ -6,6 +7,7 @@ import com.goit.javaonline.module4.TemperatureTransform;
  */
 public class Resolver {
     public static void main(String[] args) {
+        //Testing Squares
         CalcSquare sq = new CalcSquare();
         double h = 10, s = 5;
         System.out.println("Triangle h=" + h + ", s=" + s + ". Square is " + sq.TriangleSquare(h, s));
@@ -13,6 +15,8 @@ public class Resolver {
         System.out.println("Rectangle s1=" + s1 + ", s2=" + s2 + ". Square is " + sq.RectangleSquare(s1, s2));
         double r = 5;
         System.out.println("Circle r=" + r + ". Square is " + sq.CircleSquare(r));
+
+        // testing TemperatureTramsform
         TemperatureTransform t = new TemperatureTransform();
         System.out.println("Cels=" + t.getCelTemp() + ", Farenh=" + t.getFarTemp());
         double c = 5, f = 4;
@@ -20,5 +24,11 @@ public class Resolver {
         System.out.println("Cels=" + t.getCelTemp() + ", Farenh=" + t.getFarTemp());
         t.setFarTemp(f);
         System.out.println("Cels=" + t.getCelTemp() + ", Farenh=" + t.getFarTemp());
+
+        //testing Length
+        CalcLength l = new CalcLength();
+        double x1 = 3, y1 = 0, x2 = 0, y2 = 4;
+        System.out.println("Length between points (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is " + l.CalculateLength(x1, y1, x2, y2));
+
     }
 }
